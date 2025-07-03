add46466411f81a02a059f7dfda9a48dc9900146
Ninja974 <155409865+Ninja974@users.noreply.github.com>
2025-07-03 12:48:04 -0300

    Update Map 2 Public.lua

--- Map 2 Public.lua
+++ Map 2 Public.lua
@@ -1,2 +1,2 @@
 --Map 2 Public
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninja974/Fire-Scripts/refs/heads/main/Map%202%20Priv.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninja974/Codedex-Studio/refs/heads/main/Map%202%20Priv.lua"))()
